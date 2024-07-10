b9e3ac203e48d136b8a795de417900d01ee60158
PJPriyanka147 <priyankajatav07@gmail.com>
2024-07-10 18:35:47 +0530

    optimized using two pointer

--- Day-10/RearrangeArrayElementsbySign.py
+++ Day-10/RearrangeArrayElementsbySign.py
@@ -19,3 +19,24 @@ def rearrange_array(nums):
 nums = list(map(int, input("Enter Elements of array: ").split()))
 result = rearrange_array(nums)
 print(result)
+
+#using two pointer --
+def rearrange_array(nums):
+    result = [0] * len(nums)
+
+    pos_index = 0
+    neg_index = 1
+
+    for num in nums:
+        if num > 0:
+            result[pos_index] = num
+            pos_index += 2
+        else:
+            result[neg_index] = num
+            neg_index += 2
+
+    return result
+
+nums = list(map(int, input("Enter Elements of array: ").split()))
+result = rearrange_array(nums)
+print(result)

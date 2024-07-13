2828808a98166ef2d0f41790ebde38971c78f5cb
PJPriyanka147 <priyankajatav07@gmail.com>
2024-07-13 19:57:56 +0530

    hard ques, solved using brute force

---
+++ Day-12/ReversePairs.py
@@ -0,0 +1,13 @@
+#Brute Force --
+def reverse_pairs(nums):
+    count = 0
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] > 2 * nums[j]:
+                count += 1
+
+    return count
+
+nums = list(map(int, input("Enter the elements:").split()))
+result = reverse_pairs(nums)
+print(result)
